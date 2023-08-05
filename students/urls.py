334ee99ec20c953e9126b3936c3db0e0593ad86e
Mikhail Khromov <khromov05@gmail.com>
2023-08-05 20:14:57 +0300

    add student view

--- students/urls.py
+++ students/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
 
-from students.views import StudentGroupListView
+from students.views import AddStudentView, StudentGroupListView
 
 
 urlpatterns = [
     path("student-groups", StudentGroupListView.as_view(), name="student-group-list"),
+    path(
+        "student-groups/<int:pk>",
+        StudentGroupListView.as_view(),
+        name="student-group-detail",
+    ),
+    path(
+        "student-groups/<int:pk>/add-student/<int:student_id>",
+        AddStudentView.as_view(),
+        name="add-student",
+    ),
 ]
--- students/views.py
+++ students/views.py
@@ -1,8 +1,11 @@
 from rest_framework import generics
+from rest_framework import status
+from rest_framework.response import Response
 
 from students.models import StudentGroup
 from students.permissions import IsCuratorOrAdminOrReadOnly
 from students.serializers import StudentGroupSerializer
+from users.models import User
 
 
 class StudentGroupListView(generics.ListCreateAPIView):
@@ -13,3 +16,22 @@ class StudentGroupListView(generics.ListCreateAPIView):
     queryset = StudentGroup.objects.all()
     serializer_class = StudentGroupSerializer
     permission_classes = (IsCuratorOrAdminOrReadOnly,)
+
+
+class AddStudentView(generics.GenericAPIView):
+    """View to add a student to a StudentGroup."""
+
+    queryset = StudentGroup.objects.all()
+    serializer_class = StudentGroupSerializer
+
+    def post(self, request, *args, **kwargs):
+        # url = studentgroups/<int:pk>/add-student/<int:student_id>
+        student_id = self.kwargs.get("student_id")
+        try:
+            student = User.objects.get(id=student_id, user_type="S")
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        student_group = self.get_object()
+        student_group.students.add(student)
+        return Response(status=status.HTTP_200_OK)
