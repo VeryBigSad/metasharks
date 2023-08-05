22357d360d66413afd4cca51e2c8a195c583edae
Mikhail Khromov <khromov05@gmail.com>
2023-08-05 23:28:45 +0300

    fix signals

--- reports/apps.py
+++ reports/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ReportsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "reports"
+
+    def ready(self):
+        # flake8: noqa
+        import reports.signals
